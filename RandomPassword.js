d23964ed14b467548ee4606eb49a51cc05eaf168
USERNAME <EMAIL>
2023-03-02 22:58:03 -0500

    JS code added

--- RandomPassword.js
+++ RandomPassword.js
@@ -1,1 +1,11 @@
-
+function generatePassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      password += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    return password;
+  }
+  
+  console.log(generatePassword(12)); // Output: A random password with 12 characters
+  
